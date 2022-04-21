9a75140046b7068784d85e5250b32ff5ad8ac667
ManonLefevre <you@example.com>
2022-04-21 16:47:13 +0200

    maj feature

--- feature_analysis.py
+++ feature_analysis.py
@@ -1,8 +1,9 @@
 #%%
 import numpy as np
-import pandas
+import pandas as pd
+
 from load_data import *
-from preprocessing import preprocessing
+from preprocessing import *
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -23,7 +24,7 @@ def content_feature(utterances):
     same=[1 if s.find('same') != -1 or s.find('similar') != -1 else 0 for s in utterances]
     #Does the utterance contain the keywords what, where, when, why, who, how
     wh = [[1 if s.find('how') != -1 else 0,1 if s.find('what') != -1 else 0,1 if s.find('why') != -1 else 0,1 if s.find('who') != -1 else 0,1 if s.find('where') != -1 else 0,1 if s.find('when') != -1 else 0] for s in utterances]
-    return list(zip(cos_initial_utterance,cos_entire_dialogue,question,same,wh))
+    return pd.DataFrame(list(zip(cos_initial_utterance,cos_entire_dialogue,question,same,wh)), columns = ['cos_initial_utterance', 'cos_entire_dialogue','question_mark','same/similar','how/what/why/who/where/when'])
 
 #%%
 def structural_feature(utterances):
@@ -32,14 +33,15 @@ def structural_feature(utterances):
     #Normalized position of an utterance in the dialog (AbsPos divided by # utterances)
     nor_pos=[i / len(utterances) for i in range(len(utterances))]
     #Total number of words in an utterance after stop words removal
-    utterances=[preprocessing(text, low=False, punc=True, up_word=False, number=False, stem=False, stw=True, m='°') for text in utterances]
+    utterances=[stop_words_removal(text) for text in utterances]
     num_w=[len(s.split(' ')) for s in utterances]
     #Unique number of words in an utterance after stop words removal
     sw=[len(set(s.split(' '))) for s in utterances]
     #Unique number of words in an utterance after stop words removal and stemming
-    utterances=[preprocessing(text, low=False, punc=False, up_word=False, number=False, stem=True, stw=False, m='°') for text in utterances]
+    utterances=[preprocessing(text) for text in utterances]
     sw_stem=[len(set(s.split(' '))) for s in utterances]
-    return list(zip(pos,nor_pos,num_w,sw,sw_stem))
+    return pd.DataFrame(list(zip(pos,nor_pos,num_w,sw,sw_stem)), columns = ['position','normalized_position','nb_word_sw','unique_nb_word_sw','unique_nb_word_sw_stem'])
+
 
 #%%
 def sentiment_feature(utterances):
@@ -52,33 +54,29 @@ def sentiment_feature(utterances):
     #Sentiment scores of the utterance computed by VADER (positive, neutral, and negative)
     sid = SentimentIntensityAnalyzer()
     sent=[list(map(str, [sid.polarity_scores(s)['neg'], sid.polarity_scores(s)['neu'], sid.polarity_scores(s)['pos']])) for s in utterances]
-    return list(zip(thank,exc,neg,sent))
+    return pd.DataFrame(list(zip(thank,exc,neg,sent)), columns = ['thank','exclamation','did_not/does_not','pos/neu/neg'])
 
 #%%
 
-def feature_analysis(utterances):
-    feature=[]
-    for diag in utterances:
+def feature_analysis(dialogs):
+    features=[]
+    for diag in dialogs:
         content= content_feature(diag)
         structural= structural_feature(diag)
         sentiment= sentiment_feature(diag)
-        feature.append([content,structural,sentiment])
-    return feature
+        feature=content.join(structural)
+        feature=feature.join(sentiment)
+        features.append(feature)
+    return features
 
 #%%
 xt, yt, l = load_data("train",127)
 
 #%%
-xt_preprocess=[[preprocessing(text, low=True, punc=False, up_word=True, number=True, stem=False, stw=False,m='°') for text in dialogue]for dialogue in xt]
+xt_preprocess=[[preprocessing(text) for text in dialogue]for dialogue in xt]
 
 #%%
-feature= feature_analysis(xt_preprocess)
-
-#%%
-content,structural,sentiment=feature[0]
-df = pandas.DataFrame(content, columns = ['cos_initial_utterance', 'cos_entire_dialogue','question_mark','same/similar','how/what/why/who/where/when'])
-print(df.head())
-df = pandas.DataFrame(structural, columns = ['position','normalized_position','nb_word_sw','unique_nb_word_sw','unique_nb_word_sw_stem'])
-print(df.head())
-df = pandas.DataFrame(sentiment, columns = ['thank','exclamation','did_not/does_not','pos/neu/neg'])
-print(df.head())
+features=feature_analysis(xt_preprocess)
+diag=features[0]
+feature=feature_analysis(diag)
+print(feature.head())
--- preprocessing.py
+++ preprocessing.py
@@ -7,36 +7,32 @@ from nltk.corpus import stopwords
 
 sw=set(stopwords.words('english'))
 
-def preprocessing(text, low, punc, up_word, number, stem, stw, m='°'):
+def preprocessing(text):
     # suppression des accents et des caractères non normalisés
     text = unicodedata.normalize('NFD', text).encode('ascii', 'ignore').decode("utf-8")
 
     # transformation des mots entièrement en majuscule en marqueurs spécifiques
     # ici on s'intéresse aux mots de taille supérieure ou égale à 3
-    if up_word:
-        text = re.sub(r'([A-Z_0-9]{3,}[\s\n])', m + ' ', text)
+    text = re.sub(r'([A-Z_0-9]{3,}[\s\n])', '° ', text)
 
     # transformation en minuscule
-    if low:
-        text = text.lower()
-
-    # suppression de la ponctuation
-    if punc:
-        punc = string.punctuation
-        punc += '\n\r\t'
-        text = text.translate(str.maketrans(punc, ' ' * len(punc)))
+    text = text.lower()
 
     # suppression des chiffres
-    if number:
-        text = re.sub('[0-9]+', '', text)
+    text = re.sub('[0-9]+', '', text)
+    return text
 
+def stop_words_removal(text):
     # on enlève les stop words
-    if stw:
-        text = ' '.join([word for word in text.split() if word not in sw])
+    text = ' '.join([word for word in text.split() if word not in sw])
 
-    # stemming
-    if stem:
-        s = nltk.stem.SnowballStemmer('english')
-        text = ' '.join([s.stem(word) for word in text.split()])
+    # suppression de la ponctuation
+    punc = string.punctuation
+    punc += '\n\r\t'
+    text = text.translate(str.maketrans(punc, ' ' * len(punc)))
+    return text
 
+def stemming(text):
+    s = nltk.stem.SnowballStemmer('english')
+    text = ' '.join([s.stem(word) for word in text.split()])
     return text
