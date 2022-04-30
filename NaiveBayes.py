a2ce40a187d5e2532049c084d2d251439ff38777
ManonLefevre <you@example.com>
2022-04-30 10:24:40 +0200

    suite ml

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -4,12 +4,12 @@ from sklearn.calibration import CalibratedClassifierCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.naive_bayes import MultinomialNB
 from skmultilearn.problem_transform import BinaryRelevance
-from load_data import load_data, label_preprocessing_greetings, label_preprocessing_combinations
+
+from feature_analysis import feature_analysis
+from intents_preprocess import intents_preprocessing
+from load_data import *
 from preprocessing import preprocessing, stop_words_removal, stemming
 #%%
-
-
-
 
 # todo : il faut donner en datax aux modèles les features générées et pas les phrases voir ce qu'il faut donner pour
 #  les datay (est ce que binary relevance suffit)
@@ -21,50 +21,43 @@ X_test, labels_test,dico_labels_test = load_data("test",34)
 
 #%%
 ##preprocessing train
-X_train =[[stemming(stop_words_removal(preprocessing(text))) for text in dialogue]for dialogue in X_train]
-labels_preprocessed_train = label_preprocessing_greetings(labels_train)
-intents_to_keep = ['REQUEST+', 'INFORM+', 'OFFER+', 'CONFIRM+', 'SELECT+', 'GOODBYE+', 'OFFER+INFORM_COUNT+', 'INFORM_INTENT+', 'INFORM+INFORM_INTENT+', 'AFFIRM+', 'NOTIFY_SUCCESS+', 'REQ_MORE+', 'OFFER_INTENT+', 'NEGATE+', 'THANK_YOU+', 'REQUEST+AFFIRM+', 'INFORM+NOTIFY_SUCCESS+']
-Y_train = label_preprocessing_combinations(labels_preprocessed_train, intents_to_keep)
+labels_preprocessed_train =intents_preprocessing(labels_train)
+X_train_preprocess=[[preprocessing(text) for text in dialogue]for dialogue in X_train]
+features_train=feature_analysis(X_train_preprocess)
 #%%
 ##preprocessing val
-X_val =[[stemming(stop_words_removal(preprocessing(text))) for text in dialogue]for dialogue in X_val]
-labels_preprocessed_val = label_preprocessing_greetings(labels_val)
-Y_val = label_preprocessing_combinations(labels_preprocessed_val, intents_to_keep)
+labels_preprocessed_val =intents_preprocessing(labels_val)
+X_val_preprocess=[[preprocessing(text) for text in dialogue]for dialogue in X_val]
+features_val=feature_analysis(X_val_preprocess)
+#%%
 ##preprocessing test
-labels_preprocessed_test = label_preprocessing_greetings(labels_test)
-Y_test = label_preprocessing_combinations(labels_preprocessed_test, intents_to_keep)
-
+labels_preprocessed_test =intents_preprocessing(labels_test)
+X_test_preprocess=[[preprocessing(text) for text in dialogue]for dialogue in X_test]
+features_test=feature_analysis(X_test_preprocess)
 #%%
 # todo: essayer avec GaussianNB()
 clf = BinaryRelevance(MultinomialNB())
 
 #%%
-print(len(X_train))
-print(len(Y_train))
-print(len(X_train[0]))
-print(len(Y_train[0]))
-print('x', X_train[0][0])
-print('y', Y_train[0])
-#%%
 ##we use train set to fit the model
-clf.fit(X_train, Y_train)
+clf.fit(features_train, labels_preprocessed_train)
 
 #%%
 ##we use validation set to fine-tune the model hyperparameters with CalibratedClassifierCV
 #CalibratedClassifierCV : estimate the parameters of a classifier and calibrate the classifier
 #we use binaryRelevance()
-# cal_clf = CalibratedClassifierCV(base_estimator=clf, cv="prefit")
-# print(cal_clf.get_params())
-# binary_rel_clf = BinaryRelevance(cal_clf)
-# binary_rel_clf.fit(X_val,Y_val)
+cal_clf = CalibratedClassifierCV(base_estimator=clf, cv="prefit")
+print(cal_clf.get_params())
+binary_rel_clf = BinaryRelevance(cal_clf)
+binary_rel_clf.fit(features_val,labels_preprocessed_val)
 
 #%%
 ##we use Test Dataset to evaluate the model
-pred = clf.predict(X_test)
-acc=accuracy_score(Y_test,pred)
-pre=precision_score(Y_test,pred)
-rec=recall_score(Y_test,pred)
-f1=f1_score(Y_test,pred)
+pred = clf.predict(features_test)
+acc=accuracy_score(labels_preprocessed_test,pred)
+pre=precision_score(labels_preprocessed_test,pred)
+rec=recall_score(labels_preprocessed_test,pred)
+f1=f1_score(labels_preprocessed_test,pred)
 df=pd.DataFrame(['NaiveBayes',acc,pre,rec,f1],columns=['methods', 'acc', 'precision','recall','f1'])
-
+print(df)
 #%%
--- feature_analysis.py
+++ feature_analysis.py
@@ -1,12 +1,11 @@
-import numpy as np
 import pandas as pd
-
+import numpy as np
 from load_data import *
 from preprocessing import *
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
+#%%
 def content_feature(utterances):
     #tf-idf representation
     vectorizer = TfidfVectorizer()
@@ -22,13 +21,13 @@ def content_feature(utterances):
     same=[1 if s.find('same') != -1 or s.find('similar') != -1 else 0 for s in utterances]
     #Does the utterance contain the keywords what, where, when, why, who, how
     wh = [[1 if s.find('how') != -1 else 0,1 if s.find('what') != -1 else 0,1 if s.find('why') != -1 else 0,1 if s.find('who') != -1 else 0,1 if s.find('where') != -1 else 0,1 if s.find('when') != -1 else 0] for s in utterances]
-    return pd.DataFrame(list(zip(cos_initial_utterance,cos_entire_dialogue,question,same,wh)), columns = ['cos_initial_utterance', 'cos_entire_dialogue','question_mark','same/similar','how/what/why/who/where/when'])
-
+    return list(zip(cos_initial_utterance,cos_entire_dialogue,question,same,wh))
+#%%
 def structural_feature(utterances):
     #Absolute position of an utterance in the dialog
     pos=[i for i in range(len(utterances))]
     #Normalized position of an utterance in the dialog (AbsPos divided by # utterances)
-    nor_pos=[i / len(utterances) for i in range(len(utterances))]
+    nor_pos=[round(i / len(utterances),3) for i in range(len(utterances))]
     #Total number of words in an utterance after stop words removal
     utterances=[stop_words_removal(text) for text in utterances]
     num_w=[len(s.split(' ')) for s in utterances]
@@ -37,9 +36,10 @@ def structural_feature(utterances):
     #Unique number of words in an utterance after stop words removal and stemming
     utterances=[preprocessing(text) for text in utterances]
     sw_stem=[len(set(s.split(' '))) for s in utterances]
-    return pd.DataFrame(list(zip(pos,nor_pos,num_w,sw,sw_stem)), columns = ['position','normalized_position','nb_word_sw','unique_nb_word_sw','unique_nb_word_sw_stem'])
-
-
+    #Is the utterance made by the dialog starter
+    is_starter = [1 if i%2==0 else 0 for i in range(len(utterances))]
+    return list(zip(pos,nor_pos,num_w,sw,sw_stem,is_starter))
+#%%
 def sentiment_feature(utterances):
     #Does the utterance contain the keyword thank
     thank=[1 if s.find('thank') != -1 else 0 for s in utterances]
@@ -50,25 +50,28 @@ def sentiment_feature(utterances):
     #Sentiment scores of the utterance computed by VADER (positive, neutral, and negative)
     sid = SentimentIntensityAnalyzer()
     sent=[list(map(str, [sid.polarity_scores(s)['neg'], sid.polarity_scores(s)['neu'], sid.polarity_scores(s)['pos']])) for s in utterances]
-    return pd.DataFrame(list(zip(thank,exc,neg,sent)), columns = ['thank','exclamation','did_not/does_not','pos/neu/neg'])
-
-
+    return list(zip(thank,exc,neg,sent))
+#%%
 def feature_analysis(dialogs):
     features=[]
+    i=0
     for diag in dialogs:
-        content= content_feature(diag)
-        structural= structural_feature(diag)
-        sentiment= sentiment_feature(diag)
-        feature=content.join(structural)
-        feature=feature.join(sentiment)
+        print(i,"/",len(dialogs))
+        i+=1
+        content= [list(i) for i in content_feature(diag)]
+        structural= [list(i) for i in structural_feature(diag)]
+        sentiment= [list(i) for i in sentiment_feature(diag)]
+        feature= [list(i) for i in list(zip(content,structural,sentiment))]
         features.append(feature)
     return features
 
-if __name__ == '__main__':
 
-    xt, yt, l = load_data("train",127)
-    xt_preprocess=[[preprocessing(text) for text in dialogue]for dialogue in xt]
-    features=feature_analysis(xt_preprocess)
-    diag=features[0]
-    feature=feature_analysis(diag)
-    print(feature.head())
+#if __name__ == '__main__':
+#%%
+xt, yt, l = load_data("train",127)
+#%%
+xt_preprocess=[[preprocessing(text) for text in dialogue]for dialogue in xt]
+#%%
+features=feature_analysis(xt_preprocess[:10])
+diag=pd.DataFrame(features[0])
+
